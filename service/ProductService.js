f3f5a547d9decd003f802235d0be27c4129e69b4
xiongchao <1359209000@qq.com>
2020-11-15 21:22:37 +0800

    建立联表查询

--- service/ProductService.js
+++ service/ProductService.js
@@ -1,6 +1,11 @@
 const model = require('../db/model');
 
-const { Product } = model;
+const { Product, Brand } = model;
+
+Product.belongsTo(Brand, {
+  foreignKey: 'brandId',
+  targetKey: 'id' // 默认id
+});
 
 module.exports = { 
   getProducts: async ({ page = 0, size = 50, orderFileds = 'createdAt', orderRules = 'ASC' }) => {
@@ -9,7 +14,17 @@ module.exports = {
       offset: page * size,
       order: [
         [orderFileds, orderRules]
-      ]
+      ],
+      include: [
+        {
+          model: Brand, // 指定关联的model
+          attributes: [
+            ['name', 'brandName'], // 查询的关联表的字段以及字段别名
+            ['code', 'brandCoe']
+          ]
+        }
+      ],
+      raw: false // 开启原生查询，原生查询支持的功能更多，自定义更强
     });
   },
   getProductCount: async () => {
